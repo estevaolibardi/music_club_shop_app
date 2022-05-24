0ad2b3f9e18fc6e0d023bd85ca3ac11e7f056991
Johão Pedro <johaopedro99@gmail.com>
2022-05-24 12:40:38 -0300

    feat: styled component of li was created

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -3,7 +3,18 @@ import { useProducts } from "../../provider/products";
 const Shop = () => {
   const { products } = useProducts();
   console.log(products);
-  return <div>shop</div>;
+  return (
+    <div>
+      {products.map((element) => (
+        <li key={element.id}>
+          <img alt={element.name} src={element.img_url} />
+          <p>{element.name}</p>
+          <h4>{element.price}</h4>
+          <p>{element.description}</p>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Shop;
