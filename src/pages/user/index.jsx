40326e327dfa9ef4831a3df06f214d606353bda4
Johão Pedro <johaopedro99@gmail.com>
2022-05-26 14:07:49 -0300

    fix: I fixed the chat name and the profile user infos

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,7 +1,12 @@
 import Header from "../../components/Header";
 import { ContainerProfile, Container } from "./styles";
+import { UseLoginProvider } from "../../provider/login";
 
 const User = () => {
+  const { user } = UseLoginProvider();
+  const actualUser = JSON.parse(user);
+  console.log(actualUser);
+
   return (
     <div>
       <Header />
@@ -11,8 +16,10 @@ const User = () => {
             src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
             alt="foto-perfil"
           />
-          <h3>Nome</h3>
+          <h3>{actualUser.user_name}</h3>
+          <p>{actualUser.birth_date}</p>
         </ContainerProfile>
+        <div>produtos comprados</div>
       </Container>
     </div>
   );
